7ce18ac335ee508451542ad40f1942f5ad2e5403
rumesh02 <rumeshth20@gmail.com>
2026-01-29 23:43:46 +0530

    Add JWT authentication middleware and protected route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/userController");
 
+const { protect } = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 //register user
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+//protect routes
+router.get("/profile", protect, (req, res) => {
+    res.status(200).json({
+        message: "Profile accessed successfully",
+        user: req.user
+    });
+});
 
 module.exports = router;
